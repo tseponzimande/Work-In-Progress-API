70caf8d0dd420cdefdd38990358b82e07094c269
Tshepo Nzimande <Tshepo@terra.group>
2023-12-18 15:48:07 +0200

    first commit

---
+++ DevWorkAPI/DevWorkAPI/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DevWorkAPI.Models;
+namespace DevWorkAPI.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");
+        }
+    }
+}
